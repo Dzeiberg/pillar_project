8841ff127a0d3f7b947efd254fccdfef00fb36c1
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2025-02-07 15:04:34 -0500

    update dataframe parsing

--- pillar_project/data_utils/dataset.py
+++ pillar_project/data_utils/dataset.py
@@ -2,8 +2,13 @@ import pandas as pd
 from pandas.api.types import is_string_dtype
 import numpy as np
 from pathlib import Path
-
+from fire import Fire
 from functools import reduce
+import logging
+from io import StringIO
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    filename="dataset.log")
 class PillarProjectDataframe:
     def __init__(self, data_path: Path|str):
         self.data_path = Path(data_path)
@@ -30,7 +35,7 @@ def _tolist(value,sep="^"):
         return [value,]
     
 def _clean_clinsigs(values):
-    return [v.split(";")[0] for v in values]
+    return [v.split(";")[0] if isinstance(v,str) else "nan" for v in values]
 
 class Scoreset:
     def __init__(self, dataframe: pd.DataFrame,**kwargs):
@@ -56,12 +61,13 @@ class Scoreset:
         return types[0]
 
     def _init_variant(self, row,variant_type):
-        if variant_type == "nucleotide":
-            return NucleotideVariant(row)
-        elif variant_type == "aa":
-            return AminoAcidVariant(row)
-        else:
-            raise ValueError("nucleotide_or_aa must be either 'nucleotide' or 'aa'")
+        return Variant(row)
+        # if variant_type == "nucleotide":
+        #     return NucleotideVariant(row)
+        # elif variant_type == "aa":
+        #     return AminoAcidVariant(row)
+        # else:
+        #     raise ValueError("nucleotide_or_aa must be either 'nucleotide' or 'aa'")
 
     def __len__(self):
         return len(self.variants)
@@ -70,17 +76,19 @@ class Scoreset:
         NSamples = 4 # P/LP, B/LB, gnomAD, synonymous
         self._sample_assignments = np.zeros((len(self),NSamples), dtype=bool)
         missense_only = kwargs.get("missense_only",False)
+        synonymous_exclusive = kwargs.get("synonymous_exclusive",True)
         for i,variant in enumerate(self.variants):
-            if variant.is_synonymous:
+            if variant.has_synonymous():
                 self._sample_assignments[i,3] = True
-                continue
-            if not missense_only or variant.is_missense:
-                if variant.present_in_gnomAD:
-                    self._sample_assignments[i,2] = True
-                if variant.is_pathogenic_or_likely_pathogenic:
-                    self._sample_assignments[i,0] = True
-                if variant.is_benign_or_likely_benign:
-                    self._sample_assignments[i,1] = True
+                if synonymous_exclusive:
+                    continue
+            if variant.has_gnomAD(missense_only):
+                self._sample_assignments[i,2] = True
+            if variant.has_pathogenic(missense_only):
+                self._sample_assignments[i,0] = True
+            if variant.has_benign(missense_only):
+                self._sample_assignments[i,1] = True
+
     @property
     def sample_assignments(self):
         return self._sample_assignments[:,self._sample_assignments.sum(axis=0) > 0]
@@ -107,9 +115,10 @@ class Scoreset:
         return self.variants[idx]
     
     def __repr__(self):
-        out = f"Scoreset with {len(self)} variants\n"
+        out = f"Scoreset with {len(self)} total variants\n"
         for sample_scores, sample_name in self.samples:
             out += f"\t{sample_name}: {len(sample_scores)} variants\n"
+
         return out
     
 class Variant:
@@ -120,148 +129,152 @@ class Variant:
         for k, v in variant_info.items():
             setattr(self, k, v)
         self.init_gnomad_MAF()
-        self.validate_variant_info()
+        # self.validate_variant_info()
         self.parse_clinvar_sig()
+        self.parse_consequences()
+
+    def parse_consequences(self):
+        synonymous = {'synonymous_variant','synonymous','Synonymous','Silent'}
+        missense = {'Missense','missense', 'missense_variant'}
+        consequence_values = _tolist(self.consequence)
+        flagged_values = [v == "*" for v in _tolist(self.Flag)]
+        self.synonymous_annotations = np.array([v in synonymous and not f for v,f in zip(consequence_values,flagged_values)])
+        self.missense_annotations = np.array([v in missense and not f for v,f in zip(consequence_values,flagged_values)])
 
     def parse_clinvar_sig(self):
-        if hasattr(self, "clinvar_sig"):
-            self.clinvar_sig = set(_clean_clinsigs([v for v in _tolist(self.clinvar_sig) if v != "nan" and not pd.isna(v)]))
-        else:
-            self.clinvar_sig = set()
+        self.clinvar_sig_annotations = list(_clean_clinsigs([v for v in _tolist(self.clinvar_sig)]))
+        pathogenic = {"Pathogenic","Pathogenic/Likely_pathogenic","Likely_pathogenic"}
+        benign = {"Benign","Benign/Likely_benign","Likely_benign"}
+        if any([v != "nan" for v in self.clinvar_sig_annotations]):
+            logging.info(f"clinvar_sig_annotations: {self.clinvar_sig_annotations}")
+        self.pathogenic_annotation = np.array([v in pathogenic for v in self.clinvar_sig_annotations])
+        self.benign_annotation = np.array([v in benign for v in self.clinvar_sig_annotations])
 
     def init_gnomad_MAF(self):
         """
         It is possible that the MAF is a list of values separated by a semicolon. If so, parse the list and obtain the maximum value.
         """
-        maf_values = _tolist(self.gnomad_MAF)
-        maf = np.nanmax(pd.to_numeric(maf_values, errors='coerce'))
-        if np.isnan(maf):
-            maf = 0
-        self.gnomad_MAF = maf
-
-    def init_clinvar_sig(self):
-        """
-        Parse the list of strings, remove invalid values, and assign unique values to the clinvar_sig attribute as a set.
-        """
-        sigs = set([sig for sig in str(self.clinvar_sig).split(";") if \
-                not sig.startswith("CLNSIGCONF") and not sig.startswith("ID") and not sig.startswith("CLNHGVS")])
-        self.clinvar_sig = sigs
-
-    def validate_variant_info(self):
-        if not hasattr(self,'aa_ref') or not (Variant.is_nan(self.aa_ref) or self.aa_ref in self.valid_alleles.union({"*","-"})):
-            raise ValueError(f"aa_ref must be a valid amino acid, '*', '-', or NaN, not {self.aa_ref} for {self.ID}")
-        if not hasattr(self,'aa_alt') or not (Variant.is_nan(self.aa_alt) or self.aa_alt in self.valid_alleles.union({"*","-"})):
-            raise ValueError(f"aa_alt must be a valid amino acid, '*', or '-', or NaN, not {self.aa_alt} for {self.ID}")
-        if not hasattr(self, "HGNC_id"):
-            raise ValueError("HGNC_id must be given")
-        if not hasattr(self, "aa_pos"):
-            self.aa_pos = pd.to_numeric(self.aa_pos, errors='coerce')
-            raise ValueError("aa_pos must be given but can be NaN")
+        self.maf_values = pd.to_numeric(_tolist(self.gnomad_MAF),errors='coerce')
     
-    @property
-    def present_in_gnomAD(self):
-        return not pd.isna(self.gnomad_MAF) and self.gnomad_MAF > 0
+    def has_pathogenic(self,missense_only):
+        if not missense_only:
+            return any(self.pathogenic_annotation)
+        return any(self.pathogenic_annotation & self.missense_annotations)
+        pathogenic_indices = np.where(self.pathogenic_annotation)[0]
+        if not missense_only:
+            return len(pathogenic_indices) > 0
+        missense_indices = np.where(self.missense_annotations)[0]        
+        return len(set(missense_indices).intersection(set(pathogenic_indices))) > 0
     
-    @property
-    def is_pathogenic_or_likely_pathogenic(self) -> bool:
-        return len(self.clinvar_sig.intersection({"Pathogenic","Pathogenic/Likely_pathogenic","Likely_pathogenic"})) > 0
-
-    @property
-    def is_benign_or_likely_benign(self) -> bool:
-        return len(self.clinvar_sig.intersection({"Benign","Benign/Likely_benign","Likely_benign"})) > 0
-
-    @property
-    def is_conflicting(self) -> bool:
-        return len(self.clinvar_sig.intersection({"Conflicting_classifications_of_pathogenicity",})) > 0
-
-    @property
-    def is_uncertain(self) -> bool:
-        return len(self.clinvar_sig.intersection({"Uncertain_significance",})) > 0
-
-    @property
-    def valid_alleles(self):
-        return {'A','C','D','E',
-                'F','G','H','I',
-                'K','L','M','N',
-                'P','Q','R','S',
-                'T','V','W','Y'}
-        
-    @property
-    def is_missense(self):
-        if not (hasattr(self, "aa_ref") and hasattr(self, "aa_alt") and  hasattr(self, "aa_pos")):
-            return False
-        return self.aa_ref in self.valid_alleles and \
-            self.aa_alt in self.valid_alleles and \
-            self.aa_ref != self.aa_alt and \
-            self.aa_pos > 0
-        
-    @staticmethod
-    def is_nan(val):
-        return isinstance(val, float) and np.isnan(val)
-
-    @property
-    def is_synonymous(self):
-        return self.aa_ref == self.aa_alt or \
-            (self.aa_ref in self.valid_alleles and Variant.is_nan(self.aa_alt))
+    def has_benign(self,missense_only):
+        if not missense_only:
+            return any(self.benign_annotation)
+        return any(self.benign_annotation & self.missense_annotations)
+        benign_indices = np.where(self.benign_annotation)[0]
+        if not missense_only:
+            return len(benign_indices) > 0
+        missense_indices = np.where(self.missense_annotations)[0]
+        return len(set(missense_indices).intersection(set(benign_indices))) > 0
     
-    @property
-    def stop_gain(self):
-        return self.aa_ref in self.valid_alleles and self.aa_alt == "*"
+    def has_synonymous(self):
+        return any(self.synonymous_annotations)
     
-    @property
-    def stop_loss(self):
-        return self.aa_ref == "*" and self.aa_alt in self.valid_alleles
-    
-    @property
-    def stop_retained(self):
-        return (self.aa_ref == "*" and self.aa_alt == "*") or \
-            (self.aa_ref == "*" and Variant.is_nan(self.aa_alt) )
-    
-    @property
-    def noncoding(self):
-        return self.aa_ref == "-" and pd.isna(self.aa_alt)
+    def has_gnomAD(self,missense_only):
+        if not missense_only:
+            return any(self.maf_values > 0)
+        return any((self.maf_values > 0) & self.missense_annotations)
+        af_indices = np.where(self.maf_values > 0)[0]
+        if not missense_only:
+            return len(af_indices) > 0
+        missense_indices = np.where(self.missense_annotations)[0]
+        return len(set(missense_indices).intersection(set(af_indices))) > 0
     
     @property
     def score(self):
         return self.auth_reported_score
     
-      
-class AminoAcidVariant(Variant):
-    def __eq__(self,other):
-        return self.aa_ref == other.aa_ref and \
-            self.aa_alt == other.aa_alt and \
-            self.aa_pos == other.aa_pos and \
-            self.HGNC_id == other.HGNC_id
-
-class NucleotideVariant(Variant):
-    def validate_variant_info(self):
-        super().validate_variant_info()
-        return
-        if not hasattr(self, "auth_transcript_id") \
-            or not isinstance(self.auth_transcript_id, str) or \
-                not len(self.auth_transcript_id):
-            raise ValueError("auth_transcript_id must be a string")
-        if not hasattr(self, "transcript_pos"):
-            raise ValueError("transcript_pos must be provided")
-        if not hasattr(self, "transcript_ref") \
-            or not isinstance(self.transcript_ref, str) or \
-                self.transcript_ref not in self.valid_nucleotides:
-            raise ValueError(f"transcript_ref must be a valid nucleotide, not {self.transcript_ref} for {self.ID}")
-        if not hasattr(self, "transcript_alt") \
-            or not isinstance(self.transcript_alt, str) or \
-                self.transcript_alt not in self.valid_nucleotides:
-            raise ValueError(f"transcript_alt must be a valid nucleotide, not {self.transcript_alt} for {self.ID}")
-        
-    @property
-    def valid_nucleotides(self):
-        return {'A','C','G','T'}
+    @staticmethod
+    def is_nan(value):
+        return pd.isna(value) or value == "nan"
     
-    def __eq__(self,other):
-        return self.aa_ref == other.aa_ref and \
-            self.aa_alt == other.aa_alt and \
-            self.aa_pos == other.aa_pos and \
-            self.HGNC_id == other.HGNC_id and \
-            self.auth_transcript == other.auth_transcript and \
-            self.transcript_pos == other.transcript_pos and \
-            self.transcript_ref == other.transcript_ref and \
-            self.transcript_alt == other.transcript_alt
+      
+# class AminoAcidVariant(Variant):
+#     def __eq__(self,other):
+#         return self.aa_ref == other.aa_ref and \
+#             self.aa_alt == other.aa_alt and \
+#             self.aa_pos == other.aa_pos and \
+#             self.HGNC_id == other.HGNC_id
+
+# class NucleotideVariant(Variant):
+#     def validate_variant_info(self):
+#         super().validate_variant_info()
+#         return
+#         if not hasattr(self, "auth_transcript_id") \
+#             or not isinstance(self.auth_transcript_id, str) or \
+#                 not len(self.auth_transcript_id):
+#             raise ValueError("auth_transcript_id must be a string")
+#         if not hasattr(self, "transcript_pos"):
+#             raise ValueError("transcript_pos must be provided")
+#         if not hasattr(self, "transcript_ref") \
+#             or not isinstance(self.transcript_ref, str) or \
+#                 self.transcript_ref not in self.valid_nucleotides:
+#             raise ValueError(f"transcript_ref must be a valid nucleotide, not {self.transcript_ref} for {self.ID}")
+#         if not hasattr(self, "transcript_alt") \
+#             or not isinstance(self.transcript_alt, str) or \
+#                 self.transcript_alt not in self.valid_nucleotides:
+#             raise ValueError(f"transcript_alt must be a valid nucleotide, not {self.transcript_alt} for {self.ID}")
+        
+#     @property
+#     def valid_nucleotides(self):
+#         return {'A','C','G','T'}
+    
+#     def __eq__(self,other):
+#         return self.aa_ref == other.aa_ref and \
+#             self.aa_alt == other.aa_alt and \
+#             self.aa_pos == other.aa_pos and \
+#             self.HGNC_id == other.HGNC_id and \
+#             self.auth_transcript == other.auth_transcript and \
+#             self.transcript_pos == other.transcript_pos and \
+#             self.transcript_ref == other.transcript_ref and \
+#             self.transcript_alt == other.transcript_alt
+
+def summarize_datasets(dataframe_path, **kwargs):
+    """
+    Summarize the datasets in the dataframe at dataframe_path.
+
+    Parameters
+    ----------
+    dataframe_path : str
+        The path to the dataframe containing the dataset
+
+    Keyword Arguments
+    -----------------
+    - output_file : str|Path
+        The path to save the summary to
+
+    Returns
+    -------
+    None
+    """
+    output_file = kwargs.get("output_file", None)
+    if output_file is not None:
+        output_file = Path(output_file)
+        # output_file.mkdir(parents=True, exist_ok=True)
+        f = open(str(output_file),"w")
+    else:
+        f = StringIO()
+    df = PillarProjectDataframe(dataframe_path)
+    for dataset_name, ds_df in df.dataframe.groupby("Dataset"):
+        scoreset = Scoreset(ds_df,missense_only=kwargs.get("missense_only",False),synonymous_exclusive=kwargs.get("synonymous_exclusive",True))
+        f.write(f"{dataset_name}\n")
+        f.write(str(scoreset))
+        f.write("\n")
+    if isinstance(f, StringIO):
+        print(f.getvalue())
+    else:
+        f.close()
+
+
+if __name__ == "__main__":
+    # Fire(summarize_datasets)
+    summarize_datasets("/data/dzeiberg/pillar_project/dataframe/pillar_data_condensed_gold_standard_02_05_25.csv")
