1abb5939485f4ce0d0c279e13d8e29d6d7486c06
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2025-02-13 10:54:00 -0500

    update test script

--- minerva_test.sh
+++ minerva_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #BSUB -P acc_pejaverlab
 #BSUB -q premium
-#BSUB -W 143:59
+#BSUB -W 3:59
 #BSUB -J pillar_project[1-10]
-#BSUB -n 20
+#BSUB -n 10
 #BSUB -R span[hosts=1]
 #BSUB -o /sc/arion/projects/pejaverlab/users/zeibed01/pillar_project/logs/pillar_project_log.out.%I
 source /hpc/users/zeibed01/.bashrc
@@ -20,5 +20,5 @@ for dataset_num in $(seq 1 $NUM_DATASETS)
 do
         DATASET=$(sed -n "${dataset_num}p" /sc/arion/projects/pejaverlab/users/zeibed01/pillar_project/data/dataset_names.txt)
         echo "Running $DATASET - iteration $LSB_JOBINDEX";
-        python -u /sc/arion/projects/pejaverlab/users/zeibed01/pillar_project/run_fits.py run_single_fit $DATASET $DATAFRAME $SAVEDIR/fit_results_${DATASET}_${LSB_JOBINDEX}/
+        python -u /sc/arion/projects/pejaverlab/users/zeibed01/pillar_project/run_fits.py run_single_fit $DATASET $DATAFRAME $SAVEDIR/fit_results_${DATASET}_${LSB_JOBINDEX}/ --num_fits 5 --core_limit 10
 done
